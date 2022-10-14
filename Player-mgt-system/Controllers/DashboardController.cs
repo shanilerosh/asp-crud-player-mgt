f3477caf82348bbefdca1dca21a7207d29c1e5a7
Tharindu <tharindu@misynergy.com>
2022-10-15 00:52:37 +0530

    impl role base menu generation

--- Player-mgt-system/Controllers/DashboardController.cs
+++ Player-mgt-system/Controllers/DashboardController.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Player_mgt_system.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Player_mgt_system.Controllers
 {
     public class DashboardController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public DashboardController(ILogger<HomeController> logger)
+        private readonly PlayerContext _context;
+        public DashboardController(ILogger<HomeController> logger,PlayerContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,6 +22,15 @@ namespace Player_mgt_system.Controllers
             }
 
             return RedirectToAction("Index", "Login");
+        }
+        
+        public IActionResult    GetUserData()
+        {
+            HttpContext.Session.GetString("SessionVariables.SessionKeyUsername");
+            var user =
+                _context.Users.Where(obj => obj.Username.Equals(HttpContext.Session.GetString(SessionVariables.SessionKeyUsername)));
+
+            return Ok(user);
         }
 
     }
