a8e48532903128f02fff02afeccc42ea8dc98344
Shanil Irosh <shanil@misynergy.com>
2022-10-14 07:41:12 +0530

    changes login session issue

--- Player-mgt-system/Service/Impl/LoginServiceImpl.cs
+++ Player-mgt-system/Service/Impl/LoginServiceImpl.cs
@@ -19,19 +19,18 @@ public class LoginServiceImpl : ILoginService
     public async Task<IActionResult> Login(LoginDto loginDto)
     {
         
-        //check if user is 
-        // var userData = await _context.Users.FirstOrDefaultAsync(obj => obj.Username == loginDto.UserName);
-        //
-        // if (null == userData)
-        // {
-        //     throw new InvalidDataException("User not found with the username " + loginDto.UserName);
-        // }
-        //
-        // //validate if the password entered correctly
-        // if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, userData.Password))
-        // {
-        //     throw new InvalidDataException("Invalid Password for the user "+userData.Username);
-        // }
+         var userData = await _context.Users.FirstOrDefaultAsync(obj => obj.Username == loginDto.UserName);
+        
+         if (null == userData)
+         {
+             throw new InvalidDataException("User not found with the username " + loginDto.UserName);
+         }
+        
+         //validate if the password entered correctly
+         if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, userData.Password))
+         {
+             throw new InvalidDataException("Invalid Password for the user "+userData.Username);
+         }
 
         return null;
     }
