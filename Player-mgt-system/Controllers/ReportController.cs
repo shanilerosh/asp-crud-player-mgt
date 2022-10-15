ce357061c3a6fd67f97db65059ee75c636279223
Tharindu <tharindu@misynergy.com>
2022-10-15 19:46:31 +0530

    reports management impl

---
+++ Player-mgt-system/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Player_mgt_system.dto;
+using Player_mgt_system.Models;
+
+namespace Player_mgt_system.Controllers;
+
+public class ReportController : Controller
+{
+    private readonly PlayerContext _context;
+
+    public ReportController(PlayerContext context)
+    {
+        _context = context;
+    }
+    // GET
+    public IActionResult Index()
+    {
+       
+
+        return View();
+    }
+    public  IActionResult  getReportData()
+    {
+        var listTrophy =  _context.Trophy.ToList();
+        var listPlayer = _context.players.ToList();
+        var listOwner = _context.Owner.ToList();
+        var listTeam =  _context.Team.ToList();
+
+        ReportDto reportDto = new ReportDto();
+        reportDto.Owners = listOwner;
+        reportDto.PlayersList = listPlayer;
+        reportDto.Trophies = listTrophy;
+        reportDto.Teams = listTeam;
+        return Ok(reportDto);
+    }
+}
---
+++ Player-mgt-system/dto/ReportDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Player_mgt_system.Models;
+
+namespace Player_mgt_system.dto
+{
+    public class ReportDto
+    {
+        public List<Trophy>Trophies { get; set; }
+        public List<Player>PlayersList { get; set; }
+        public List<Owner>Owners { get; set; }
+        public List<Team>Teams { get; set; }
+    }
+}
